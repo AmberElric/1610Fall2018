2a4bd84a207c5445c05cde1371b0db742b95b170
Katherine <pussinbootsthe3rd@gmail.com>
2018-09-17 14:33:40 -0600

    added scripts and moving

---
+++ 1610/Assets/move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public CharacterController Character;
+	public float Speed;
+	public Vector3 RotateSpeed;
+	public Vector3 moveSpeed;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		moveSpeed.x = Speed*Input.GetAxis("Horizontal");
+		moveSpeed *= Time.deltaTime;
+
+		Character.Move(moveSpeed);
+
+	}
+}

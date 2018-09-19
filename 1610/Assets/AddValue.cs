4b76eb6e1a187dafafe5c3fd5056920f23d86106
Katherine <pussinbootsthe3rd@gmail.com>
2018-09-19 15:40:49 -0600

    AddedHealthAndMoney

---
+++ 1610/Assets/AddValue.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+[CreateAssetMenu]
+public class AddValue : ScriptableObject
+{
+    public FloatData ValueObject;
+    public UnityEvent EventMin;
+    public UnityEvent EventMax;
+    public FloatData MaxValue;
+    public FloatData MinValue;
+    public void AddValueToObject(FloatData data)
+    {
+        ValueObject.Value += data.Value;
+
+        if (ValueObject.Value >= MaxValue.Value)
+        {
+            EventMax.Invoke();
+            
+        }
+        
+        if (ValueObject.Value <= MinValue.Value)
+        {
+            EventMin.Invoke();
+            
+        }
+        
+    }
+    
+}

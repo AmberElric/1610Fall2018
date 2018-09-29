a0fa4faa8a5244da1887889c098e18a1f9e0eee8
Katherine <pussinbootsthe3rd@gmail.com>
2018-09-28 19:47:36 -0600

    CodeThat'sStupidAndDoesn'tWorkTheWayIWant

---
+++ 1610/Assets/code/MuiltValue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+[CreateAssetMenu]
+public class MuiltValue : ScriptableObject
+{
+
+    public FloatData Multi;
+
+    public void OnMulitValue(FloatData outsideData)
+    {
+        outsideData.Value *= Multi.Value;
+    }
+}

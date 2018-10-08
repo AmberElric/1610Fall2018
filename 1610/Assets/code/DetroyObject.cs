849f19b22800c935d7a5e21782f226af92c9adc9
Katherine <pussinbootsthe3rd@gmail.com>
2018-10-08 15:30:44 -0600

    AddedBullets

---
+++ 1610/Assets/code/DetroyObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetroyObject : MonoBehaviour
+{
+
+	// Use this for initialization
+	IEnumerator Start()
+	
+	{
+		yield return new WaitForSeconds(5);
+Destroy(gameObject);
+		
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		Destroy(gameObject);
+		Destroy(other.gameObject);
+	}
+}

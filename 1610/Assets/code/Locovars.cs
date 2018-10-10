a554904c44c62f9cdf3470728f2ca6ca5bd0be95
Katherine <pussinbootsthe3rd@gmail.com>
2018-10-10 15:36:17 -0600

    IDon'tKnow
    local variables

---
+++ 1610/Assets/code/Locovars.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locovars : MonoBehaviour
+{
+
+	public string Name;
+
+	public void Call()
+	{
+		Name = "Bobbie";
+		print(Name);
+	}
+
+	public void Call(string newName)
+	{
+		print(newName);
+		print(Name);
+	}
+
+}
---
+++ 1610/Assets/code/WhatCanSeeIt.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhatCanSeeIt  : ScriptableObject
+{
+
+    private float health;
+    protected float Powerlevel;
+    public float EvergyLevel;
+
+    private void AddHealth()
+    {
+       health += 100 
+    }
+
+    protected void AddPower()
+    {
+        AddHealth();
+    }
+
+    public void AddEnergy()
+    {
+        
+    }
+}
+
+public class ICanSeeIt : MonoBehaviour
+{
+    private WhatCanSeeIt newObject;
+
+
+    private void Start()
+    {
+        newObject.EvergyLevel = 100;
+        newObject.AddEnergy();
+    }
+}
+
+public class Child : WhatCanSeeIt
+{
+    void callThings()
+    {
+        Powerlevel = 200;
+        EvergyLevel = 300;
+        AddPower();
+        AddEnergy();
+    }
+}

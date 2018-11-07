3f2310be84b2450e5f6e4731157fb30edb8dde4d
Katherine <pussinbootsthe3rd@gmail.com>
2018-11-07 15:23:18 -0700

    Added While loops

---
+++ 1610/Assets/code/EventRepeat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventRepeat : MonoBehaviour
+{
+	public float Seconds = 2;
+	public UnityEvent[] OnRepeat;
+	public UnityEvent OnRepeatTwo;
+
+	private int i = 0;
+	// Use this for initialization
+	IEnumerator Start () {
+		while (true)
+		{
+			yield return new WaitForSeconds(Seconds);
+
+			OnRepeat[i].Invoke();
+			if (i < OnRepeat.Length - 1)
+			{
+				i++;
+
+			}
+			else
+			{
+				i = 0;
+			}
+		}
+	}
+	
+	
+
+}
